e605855caca1eabc2d6031ffe408d9c085be76d5
hemalrika <hemalrika@gmail.com>
2022-10-18 23:10:06 +0600

    updated

--- wp-content/plugins/element-helper/widgets/slider/slider-widget.php
+++ wp-content/plugins/element-helper/widgets/slider/slider-widget.php
@@ -161,16 +161,24 @@ class Slider extends Element_El_Widget {
 		?>
 		<?php if ( $settings['design_style'] === 'style-1' ): ?>
 			<?php if(!empty($settings['slides'])) : ?>
-			<div class="nivo-slider-wrapper">
-				<div id="slider" class="nivoSlider">
+            <div id="swiper_slider" class="swiper_slider swiper-container swiper">
+				<div class="swiper-wrapper">
 					<?php foreach($settings['slides'] as $slide) :?>
+					<div class="swiper-slide">
 						<a href="<?php echo esc_url($slide['slide_url']['url']) ? esc_url($slide['slide_url']['url']) : ''; ?>">
 							<img src="<?php echo esc_url($slide['slide_image']['url']) ?>" alt="img">
 						</a>
+					</div> 
 					<?php endforeach; ?>
 				</div>
-			</div>
-			<?php endif; ?>
+				<div class="slider-image-pagination"></div>
+				<div class="slider-image-navigation">
+				<span class="image-prev"><i class="fa-regular fa-angle-left"></i></span>
+					<span class="image-next"><i class="fa-regular fa-angle-right"></i></span>
+					
+				</div>
+            </div>
+            <?php endif; ?>
 		<?php endif; ?>
 		<?php
 	}
